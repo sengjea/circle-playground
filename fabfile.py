66fff7ab9ee98cfb5082e8681d9b5ce78f6c002d
Seng Jea Lee <sengjea.lee@unrulygroup.com>
2019-06-12 16:25:52 +0100

    JSON this thing

--- fabfile.py
+++ fabfile.py
@@ -10,11 +10,9 @@ from fabric.api import *
 from time import sleep
 
 event = {
-    what: "Circle CI",
-    tags: ["test"],
-    data: "Data sent from CircleCI"
-
-
+    "what": "Circle CI",
+    "tags": ["test"],
+    "data": "Data sent from CircleCI"
 }
 
 req = requests.post(url="{base}/events/".format(base=os.environ['URL']), json=event, auth=(os.environ['USER'], os.environ['PASS'])) 
