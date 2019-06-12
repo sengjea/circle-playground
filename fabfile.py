608f52fa0c808f082474257a737d0de232cc1836
Seng Jea Lee <sengjea.lee@unrulygroup.com>
2019-06-12 12:16:58 +0100

    print response

--- fabfile.py
+++ fabfile.py
@@ -9,4 +9,5 @@ import time
 from fabric.api import *
 from time import sleep
 
-requests.post(url=os.environ['URL'], auth=(os.environ['USER'], os.environ['PASS'])) 
+req = requests.post(url=os.environ['URL'], auth=(os.environ['USER'], os.environ['PASS'])) 
+print(req.text)
